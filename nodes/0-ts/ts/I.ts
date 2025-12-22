b6ee06f686f6aa7f9e10bbd1dd541c26ae73eb41
s0fractal <sergey.glova@gmail.com>
2025-12-22 16:05:24 +0200

    Δ Mirroring: Topology Transmutation (Hyper-Compression Activated)

--- nodes/0-ts/ts/I.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 🛑 DO NOT EDIT. GENERATED CACHE.
- * Source: glyphs/intents/I.sigma
- *
- * Identity (I)
- * The fundamental particle of existence.
- */
-
-export const I = <T>(x: T): T => x;
