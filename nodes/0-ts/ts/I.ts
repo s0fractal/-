c41a49ddc53c2b3df88a001d50cc4c4f0d465038
s0fractal <sergey.glova@gmail.com>
2025-12-22 07:45:03 +0200

    ⋈ Nexus: Synced glyphs, sh and nodes

---
+++ nodes/0-ts/ts/I.ts
@@ -0,0 +1,9 @@
+/**
+ * 🛑 DO NOT EDIT. GENERATED CACHE.
+ * Source: glyphs/intents/I.sigma
+ *
+ * Identity (I)
+ * The fundamental particle of existence.
+ */
+
+export const I = <T>(x: T): T => x;
